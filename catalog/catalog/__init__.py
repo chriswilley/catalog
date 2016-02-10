033f59e06703118e84afb7e8a8d798724c689939
Chris Willey <cwilley@gmail.com>
2016-02-10 09:32:21 -0500

    Added application logging.

--- catalog/catalog/__init__.py
+++ catalog/catalog/__init__.py
@@ -1,7 +1,11 @@
+import logging
+
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 from flask_debugtoolbar import DebugToolbarExtension
+from logging import Formatter, getLogger
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__, instance_relative_config=True)
 
@@ -11,6 +15,17 @@ app.config.from_pyfile('config.py')
 db = SQLAlchemy(app)
 
 toolbar = DebugToolbarExtension(app)
+
+if not (app.debug):
+    logfile = app.config['LOG_FILE']
+    handler = RotatingFileHandler(logfile, maxBytes=10240, backupCount=10)
+    handler.setLevel(logging.WARNING)
+    handler.setFormatter(Formatter(
+        '%(asctime)s %(levelname)s %(message)s [in %(pathname)s:%(lineno)d]'
+    ))
+    loggers = [app.logger, getLogger('sqlalchemy')]
+    for l in loggers:
+        l.addHandler(handler)
 
 from catalog.views.auth import auth
 from catalog.views.home import home
